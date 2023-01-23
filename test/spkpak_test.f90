4c344ccbb6a7e05f0a1a02dc4111d4c759901e81
Jacob Williams <jacobwilliams@users.noreply.github.com>
2023-01-23 00:33:53 -0600

    updated unittest

--- test/spkpak_test.f90
+++ test/spkpak_test.f90
@@ -15,18 +15,21 @@
 
     integer,parameter :: ndim = 1    !! 1d problem
     integer(ip),parameter :: nxdata = 20 !! number of points in x
-    real(wp),dimension(1),parameter :: wdata = [-1.0_wp] !! don't use weights
     integer,dimension(ndim),parameter :: nodes = [10]
     integer,parameter :: ncol = product(nodes)
     integer,parameter :: nwrk = ncol*(ncol+1) + 1   ! is doc wrong? do we need the + 1 (otherwise, it fails)...
     integer,parameter :: ncf = ncol
+    integer(ip),parameter :: nxdata_est = 100 !! number of points for estimate plot
 
     real(wp),dimension(ndim,nxdata) :: xdata
-    real(wp),dimension(nxdata) :: ydata, ydata_est
+    real(wp),dimension(nxdata) :: ydata
     real(wp),dimension(ndim)   :: xmin
     real(wp),dimension(ndim)   :: xmax
     real(wp),dimension(nwrk)   :: work
     real(wp),dimension(ncf)    :: coef
+    real(wp),dimension(nxdata) :: wdata !! weights
+    real(wp),dimension(nxdata_est) :: xdata_est, ydata_est
+
     integer :: ierror, istat
     integer :: i !! counter
     real(wp) :: xtrap
@@ -47,13 +50,13 @@
     do i=1,nxdata
         call random_number(r)
         r = (r - 0.5_wp)/ 10.0_wp   ! some random noise
-
+        wdata(i) = 1.0_wp - abs(r) ! weight
         xdata(1,i) = real(i-1,wp)/real(nxdata-1,wp)
         ydata(i) = f1(xdata(1,i)) + r
     end do
 
-    write(*,'(a,*(f8.3,","))') 'xdata = ', xdata
-    write(*,'(a,*(f8.3,","))') 'ydata = ', ydata
+    ! write(*,'(a,*(f8.3,","))') 'xdata = ', xdata
+    ! write(*,'(a,*(f8.3,","))') 'ydata = ', ydata
 
     ! initialize:
     call splcw(1,xdata,1,ydata,wdata,nxdata,xmin,xmax, &
@@ -64,16 +67,20 @@
 
     ! compute max error at interpolation points
     errmax = 0.0_wp
-    do i=1,nxdata
-        f = splfe (ndim,xdata(1,i),coef,xmin,xmax,nodes,ierror)
+    do i=1,nxdata_est
+        xdata_est(i) = real(i-1,wp)/nxdata_est
+        f = splfe (ndim,xdata_est(i),coef,xmin,xmax,nodes,ierror)
         ydata_est(i) = f
         if (ierror /= 0) error stop 'error calling splfe'
-        tru    = f1(xdata(1,i))
+        tru    = f1(xdata_est(i))
         err    = abs(tru-f)
         errmax = max(err,errmax)
     end do
     write(*,*) 'splfe errmax = ', errmax
     if (abs(errmax)>1.0e-1_wp) error stop 'errmax too large'
+
+    ! write(*,'(a,*(f8.3,","))') 'xdata_est = ', xdata_est
+    ! write(*,'(a,*(f8.3,","))') 'ydata_est = ', ydata_est
 
     write(nodes_str,'(I10)') nodes(1); nodes_str = adjustl(nodes_str)
 
@@ -85,11 +92,10 @@
     call plt%add_plot(xdata(1,:),ydata,&
                         label='Original points',&
                         linestyle='ko',markersize=5,linewidth=2,istat=istat)
-    call plt%add_plot(xdata(1,:),ydata_est,&
+    call plt%add_plot(xdata_est,ydata_est,&
                         label='Least squares bspline with '//trim(nodes_str)//' nodes',&
                         linestyle='r-',markersize=2,linewidth=2,istat=istat)
     call plt%savefig(pyfile='splpak_test.py', figfile='splpak_test.png',istat=istat)
-
 
     contains
 
