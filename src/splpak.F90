d36e6211bb9a667e00beca6760aa84ff9c18d216
jacobwilliams <jacobwilliams@users.noreply.github.com>
2023-01-25 04:54:38 +0000

    Deploying to gh-pages from @ jacobwilliams/splpak@d6fb28db90968b566979356137e02e93d7917d13 🚀

--- src/splpak.F90
+++ src/splpak.F90
@@ -24,12 +24,23 @@
 
     module splpak_module
 
-    use iso_fortran_env, only: output_unit
-    use iso_fortran_env, only: wp => real64
+    use iso_fortran_env
 
     implicit none
 
     private
+
+#ifdef REAL32
+    integer,parameter :: wp = real32   !! Real working precision [4 bytes]
+#elif REAL64
+    integer,parameter :: wp = real64   !! Real working precision [8 bytes]
+#elif REAL128
+    integer,parameter :: wp = real128  !! Real working precision [16 bytes]
+#else
+    integer,parameter :: wp = real64   !! Real working precision if not specified [8 bytes]
+#endif
+
+    integer,parameter,public :: splpak_wp = wp   !! Working precision
 
     type,public :: splpak_type
 
